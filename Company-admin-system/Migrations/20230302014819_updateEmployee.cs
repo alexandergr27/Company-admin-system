4fd7f19b669b350fe265d0d717a9a9829a87c0c6
Alexander Guzman Rodriguez <alex_guzmanrodriguez_7@outlook.com>
2023-03-01 20:57:55 -0500

    Refactor: update employee model

---
+++ Company-admin-system/Migrations/20230302014819_updateEmployee.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Company_admin_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "lastName",
+                table: "employees",
+                newName: "LastName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                table: "employees",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "employees",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "employees",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "employees",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "salary",
+                table: "employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "employees");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "employees");
+
+            migrationBuilder.DropColumn(
+                name: "salary",
+                table: "employees");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "employees",
+                newName: "lastName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                table: "employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastName",
+                table: "employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+        }
+    }
+}
--- Company-admin-system/Models/Employee.cs
+++ Company-admin-system/Models/Employee.cs
@@ -8,12 +8,23 @@ namespace Company_admin_system.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(80)]
         public string name { get; set; }
 
-        public string lastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MaxLength(80)]
+        public string LastName { get; set; }
+
+        [MaxLength(80)]
+        public string Position { get; set; }
+
+        public int salary { get; set; }
 
         public DateTime dayOfBirth { get; set; }
-
+        
+        [MaxLength(10)]
         public string phone { get; set; }
 
         public DateTime createdAt { get; set; } = DateTime.Now;
