7719feb3c20133a597b468437f2c99ba7c5a942c
Alexander Guzman Rodriguez <alex_guzmanrodriguez_7@outlook.com>
2023-03-07 21:40:40 -0500

    Feat: Create EmployeeController, the view in which the employees are listed on a table

---
+++ Company-admin-system/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Company_admin_system.Data;
+using Company_admin_system.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company_admin_system.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Employee> employees = _db.employees;
+            return View(employees);
+        }
+    }
+}
